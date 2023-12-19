32e56c5efc7b6b1f9b8f9f9bf7884a4bb9c61d8c
Lavante Cotton <lavanteisaac@gmail.com>
2023-12-19 12:49:10 -0500

    passed

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 // Code your solutions in this file
+
+for (let age = 30; age < 40; age++) {
+  console.log(`I'm ${age} years old. Happy birthday to me!`);
+
+}
+
+const gifts = ["teddy bear", "drone", "doll"];
+
+function wrapGifts(gifts) {
+ let thankYou
+    console.log(`Wrapped ${gifts[i]} and added a bow!`);
+
+  
+
+  return gifts;}
+
+
+wrapGifts(gifts);
+
+
+function writeCards(names){
+let thankYouMessage =[]
+names.forEach(function(name) {
+    let message = `Thank you, ${name}, for the wonderful surprise gift!`;
+    thankYouMessage.push(message);
+})
+ return thankYouMessage
+
+}
+
+let names = ["Charlie", "Samip", "Ali"];
+let messages = writeCards(names);
+console.log(messages);
+
+
+
+function countDown(number){
+while (number >= 0){
+  console.log(number--)
+}
+
+}
